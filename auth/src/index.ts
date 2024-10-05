18403f224679f32d13a330d46985c9fdd3e96d19
vicky <vickykumarsunilja@gmail.com>
2024-10-05 14:26:29 +0530

    initialized the test environment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,14 +6,17 @@ const start = async () => {
     throw new Error("JWT_KEY must be defined");
   }
 
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to mongodb");
   } catch (error) {
     console.error(error);
   }
   app.listen(3000, () => {
-    console.log("Listening on port 3000!!");
+    console.log("Listening auth on port 3000!!");
   });
 };
 
