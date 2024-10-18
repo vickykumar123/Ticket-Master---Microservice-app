51c35a0732207a2e54a590314d65fe13416a0ecf
vicky <vickykumarsunilja@gmail.com>
2024-10-18 19:24:52 +0530

    Initialized the expiration service

--- ticket/src/routes/updateTicket.ts
+++ ticket/src/routes/updateTicket.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestError,
   NotAuthorizedError,
   NotFoundError,
   requireAuth,
@@ -26,6 +27,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError("Ticket is reservered, You can't edit...");
     }
 
     if (ticket.userId !== req.currentUser?.id) {
